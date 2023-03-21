3f7a94fd628f9a1890f340f8724b45d6eb850c48
Michael-Anderson-19 <michael.i.anderson@outlook.com>
2023-03-21 21:40:57 +0000

    Added method body for tests

--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -7,8 +7,12 @@ import org.junit.rules.ExpectedException;
 import org.mockito.*;
 import thirdparty.paymentgateway.TicketPaymentService;
 import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.eq;
 
 public class TicketServiceImplTest {
 
@@ -32,30 +36,87 @@ public class TicketServiceImplTest {
     }
 
     @Test
-    public void given_ValidAccountIDAndValidTicketRequests_When_callingPurchaseTickets_Then_ReserveSeatsAndMakePaymentAreCalled() {
+    public void given_ValidAccountIDAndValidTicketRequests_When_callingPurchaseTickets_Then_ReserveSeatsAndMakePaymentAreCalled(){
+        TicketTypeRequest adultTicket = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
+        TicketTypeRequest childTicket = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2);
+        TicketTypeRequest infantTicket = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 3);
 
+        ticketService.purchaseTickets(VALID_ACCOUNT_ID, adultTicket, childTicket, infantTicket);
+
+        Mockito.verify(seatReservationService, Mockito.times(1)).reserveSeat(any(Long.class), anyInt());
+        Mockito.verify(ticketPaymentService, Mockito.times(1)).makePayment(any(Long.class), anyInt());
     }
 
     @Test
-    public void given_InValidAccountID_When_callingPurchaseTickets_Then_ThrowException(){}
+    public void given_InValidAccountID_When_callingPurchaseTickets_Then_ThrowException(){
+        TicketTypeRequest ticket1 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 2);
+
+        thrown.expect(InvalidPurchaseException.class);
+        thrown.expectMessage("Invalid account"); //TODO change
+        ticketService.purchaseTickets(INVALID_ACCOUNT_ID, ticket1);
+    }
 
     @Test
-    public void given_TicketRequestWithNoTickets_When_CallingPurchaseTickets_Then_ThrowException(){}
+    public void given_TicketRequestWithNoTickets_When_CallingPurchaseTickets_Then_ThrowException(){
+        TicketTypeRequest ticket1 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 0);
+
+        thrown.expect(InvalidPurchaseException.class);
+        thrown.expectMessage("Invalid ticket request"); //TODO change
+        ticketService.purchaseTickets(VALID_ACCOUNT_ID, ticket1);
+    }
 
     @Test
-    public void given_21TotalTickets_When_CallingPurchaseTickets_Then_ThrowException() {}
+    public void given_21TotalTickets_When_CallingPurchaseTickets_Then_ThrowException() {
+        TicketTypeRequest ticket1 = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 21);
+
+        thrown.expect(InvalidPurchaseException.class);
+        thrown.expectMessage("Invalid ticket request");
+
+        ticketService.purchaseTickets(VALID_ACCOUNT_ID, ticket1);
+    }
 
     @Test
-    public void given_TicketRequestsWithNoAdult_When_CallingPurchaseTickets_Then_ThrowException(){}
+    public void given_TicketRequestsWithNoAdultTicket_When_CallingPurchaseTickets_Then_ThrowException(){
+        TicketTypeRequest childTickets = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 2);
+        TicketTypeRequest infantTickets = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 1);
+
+        thrown.expect(InvalidPurchaseException.class);
+        thrown.expectMessage("Invalid ticket request - No adult is present"); //TODO implement
+
+        ticketService.purchaseTickets(VALID_ACCOUNT_ID, childTickets,infantTickets);
+    }
 
     @Test
-    public void given_InfantAndChildAndAnAdultTicketRequests_When_CallingPurchaseTickets_Then_InfantTicketsDoNotIncreasePrice(){}
+    public void given_InfantAndChildAndAnAdultTicketRequests_When_CallingPurchaseTickets_Then_InfantTicketsDoNotIncreasePrice(){
+        TicketTypeRequest adultTicketRequest = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 5);
+        TicketTypeRequest childTicketRequest = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 3);
+        TicketTypeRequest infantTicketRequest = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 5);
+
+        final int predictedCost = TicketServiceImpl.ADULT_TICKET_COST * adultTicketRequest.getNoOfTickets()
+                + TicketServiceImpl.CHILD_TICKET_COST * childTicketRequest.getNoOfTickets();
+
+        ticketService.purchaseTickets(VALID_ACCOUNT_ID, adultTicketRequest, childTicketRequest, infantTicketRequest);
+
+        Mockito.verify(ticketPaymentService, Mockito.times(1)).makePayment(eq(VALID_ACCOUNT_ID), integerCaptor.capture());
+
+        int actualCost = integerCaptor.getValue();
+        assert(predictedCost == actualCost);
+    }
 
     @Test
-    public void given_InfantAndChildAndAdultTicketRequests_When_CallingPurchaseTickets_Then_InfantTicketsDoNotIncreaseTotalSeatCount() {}
+    public void given_InfantAndChildAndAdultTicketRequests_When_CallingPurchaseTickets_Then_InfantTicketsDoNotIncreaseTotalSeatCount() {
+        TicketTypeRequest adultTicketRequest = new TicketTypeRequest(TicketTypeRequest.Type.ADULT, 3);
+        TicketTypeRequest childTicketRequest = new TicketTypeRequest(TicketTypeRequest.Type.CHILD, 6);
+        TicketTypeRequest infantTicketRequest = new TicketTypeRequest(TicketTypeRequest.Type.INFANT, 5);
 
+        final int predictedSeats = adultTicketRequest.getNoOfTickets() + childTicketRequest.getNoOfTickets();
 
+        ticketService.purchaseTickets(VALID_ACCOUNT_ID, adultTicketRequest, childTicketRequest, infantTicketRequest);
 
+        Mockito.verify(ticketPaymentService, Mockito.times(1)).makePayment(eq(VALID_ACCOUNT_ID), anyInt());
+        Mockito.verify(seatReservationService, Mockito.times(1)).reserveSeat(eq(VALID_ACCOUNT_ID), integerCaptor.capture());
 
-
+        final int actualSeatCount = integerCaptor.getValue();
+        assert (predictedSeats == actualSeatCount);
+    }
 }
