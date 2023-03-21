ab7cf9b41df48979c6ef2c03af74e64e3f5aae33
Michael-Anderson-19 <michael.i.anderson@outlook.com>
2023-03-21 21:01:39 +0000

    Added dependencies to test file.

--- src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
+++ src/test/java/uk/gov/dwp/uc/pairtest/TicketServiceImplTest.java
@@ -1,17 +1,34 @@
 package uk.gov.dwp.uc.pairtest;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.*;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 
 import static org.junit.Assert.*;
 
 public class TicketServiceImplTest {
 
-
+    @Mock
+    private TicketPaymentService ticketPaymentService;
+    @Mock
+    private SeatReservationService seatReservationService;
+    @InjectMocks
     private TicketServiceImpl ticketService;
+    @Captor
+    ArgumentCaptor<Integer> integerCaptor;
+    @Rule
+    private final ExpectedException thrown = ExpectedException.none();
+
+    private final Long VALID_ACCOUNT_ID = 1L;
+    private final Long INVALID_ACCOUNT_ID = -1L;
 
     @Before
     public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
