22a09925e6ddfa189bc61ab680dd4ae9da6ff574
Michael-Anderson-19 <michael.i.anderson@outlook.com>
2023-03-21 21:59:21 +0000

    implemented purchaseTickets and private helper methods.

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -27,7 +27,19 @@ public class TicketServiceImpl implements TicketService {
 
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        if (!validateAccount(accountId)) throw new InvalidPurchaseException("Invalid account");
 
+        if (!validateTotalTicketCount(ticketTypeRequests))
+            throw new InvalidPurchaseException("Invalid ticket request - number of tickets must be between 1 and 20");
+
+        if (!validateAdultIsPresent(ticketTypeRequests))
+            throw new InvalidPurchaseException("Invalid ticket request - All ticket requests must contain at least 1 adult");
+
+        makePayment(accountId,
+                calculateTotalRequestsCost(ticketTypeRequests));
+
+        reserveSeats(accountId,
+                calculateTotalSeats(ticketTypeRequests));
     }
 
     private void makePayment(Long accountId, int amount) {
@@ -37,5 +49,48 @@ public class TicketServiceImpl implements TicketService {
     private void reserveSeats(Long accountId, int numberOfSeats) {
         seatReservationService.reserveSeat(accountId, numberOfSeats);
     }
+
+    private boolean validateTotalTicketCount(TicketTypeRequest[] ticketTypeRequests) {
+        int totalTickets = calculateTotalTickets(ticketTypeRequests);
+        return ((totalTickets <= 20) && (totalTickets > 0));
+    }
+
+    private boolean validateAdultIsPresent(TicketTypeRequest[] ticketTypeRequests) {
+        return Arrays.stream(ticketTypeRequests)
+                .anyMatch(ticket -> ticket.getTicketType() == TicketTypeRequest.Type.ADULT);
+    }
+
+    private boolean validateAccount(Long accountId) {
+        return accountId > 0;
+    }
+
+    private int calculateTotalTickets(TicketTypeRequest[] ticketTypeRequests) {
+        return Arrays.stream(ticketTypeRequests).mapToInt(TicketTypeRequest::getNoOfTickets)
+                .sum();
+    }
+
+    private int calculateTotalSeats(TicketTypeRequest[] ticketTypeRequests) {
+        return Arrays.stream(ticketTypeRequests).filter(ticket ->
+                        !ticket.getTicketType().equals(TicketTypeRequest.Type.INFANT))
+                .mapToInt(TicketTypeRequest::getNoOfTickets)
+                .sum();
+    }
+
+    private int calculateTotalRequestsCost(TicketTypeRequest[] ticketTypeRequests) {
+        return Arrays.stream(ticketTypeRequests).mapToInt(this::getTotalTicketCost).sum();
+    }
+
+    private int getTotalTicketCost(TicketTypeRequest ticket) {
+        return getTicketCost(ticket.getTicketType()) * ticket.getNoOfTickets();
+    }
+
+    private int getTicketCost(TicketTypeRequest.Type type) {
+        return switch (type) {
+            case INFANT -> INFANT_TICKET_COST;
+            case CHILD -> CHILD_TICKET_COST;
+            case ADULT -> ADULT_TICKET_COST;
+        };
+    }
+
 
 }
