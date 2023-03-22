99f17d55379b4b01c9b25423eabef6f3fd933175
Michael-Anderson-19 <michael.i.anderson@outlook.com>
2023-03-22 20:02:18 +0000

    Added javadoc comments to methods.

--- src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -25,6 +25,12 @@ public class TicketServiceImpl implements TicketService {
         this.seatReservationService = seatReservationService;
     }
 
+    /**
+     *
+     * @param accountId
+     * @param ticketTypeRequests
+     * @throws InvalidPurchaseException
+     */
     @Override
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
         if (!validateAccount(accountId)) throw new InvalidPurchaseException("Invalid account");
@@ -42,33 +48,68 @@ public class TicketServiceImpl implements TicketService {
                 calculateTotalSeats(ticketTypeRequests));
     }
 
+    /**
+     *
+     * @param accountId
+     * @param amount
+     */
     private void makePayment(Long accountId, int amount) {
         ticketPaymentService.makePayment(accountId, amount);
     }
 
+    /**
+     *
+     * @param accountId
+     * @param numberOfSeats
+     */
     private void reserveSeats(Long accountId, int numberOfSeats) {
         seatReservationService.reserveSeat(accountId, numberOfSeats);
     }
 
+    /**
+     *
+     * @param ticketTypeRequests
+     * @return if the total number of tickets in the provided array of ticketRequests is within the accepted range.
+     */
     private boolean validateTotalTicketCount(TicketTypeRequest[] ticketTypeRequests) {
         int totalTickets = calculateTotalTickets(ticketTypeRequests);
         return ((totalTickets <= 20) && (totalTickets > 0));
     }
 
+    /**
+     *
+     * @param ticketTypeRequests
+     * @return if an adult ticket is present within the array of ticketRequests.
+     */
     private boolean validateAdultIsPresent(TicketTypeRequest[] ticketTypeRequests) {
         return Arrays.stream(ticketTypeRequests)
                 .anyMatch(ticket -> ticket.getTicketType() == TicketTypeRequest.Type.ADULT);
     }
 
+    /**
+     *
+     * @param accountId
+     * @return if the provided account ID is valid.
+     */
     private boolean validateAccount(Long accountId) {
         return accountId > 0;
     }
 
+    /**
+     *
+     * @param ticketTypeRequests
+     * @return total number of tickets in the given array of ticketRequests.
+     */
     private int calculateTotalTickets(TicketTypeRequest[] ticketTypeRequests) {
         return Arrays.stream(ticketTypeRequests).mapToInt(TicketTypeRequest::getNoOfTickets)
                 .sum();
     }
 
+    /**
+     *
+     * @param ticketTypeRequests
+     * @return total number of seats for the given array of ticketRequests, excludes tickets for infants.
+     */
     private int calculateTotalSeats(TicketTypeRequest[] ticketTypeRequests) {
         return Arrays.stream(ticketTypeRequests).filter(ticket ->
                         !ticket.getTicketType().equals(TicketTypeRequest.Type.INFANT))
@@ -76,14 +117,29 @@ public class TicketServiceImpl implements TicketService {
                 .sum();
     }
 
+    /**
+     *
+     * @param ticketTypeRequests
+     * @return total cost of all tickets for the given array of ticketRequests.
+     */
     private int calculateTotalRequestsCost(TicketTypeRequest[] ticketTypeRequests) {
         return Arrays.stream(ticketTypeRequests).mapToInt(this::getTotalTicketCost).sum();
     }
 
+    /**
+     *
+     * @param ticket
+     * @return the total cost of a single ticketRequest.
+     */
     private int getTotalTicketCost(TicketTypeRequest ticket) {
         return getTicketCost(ticket.getTicketType()) * ticket.getNoOfTickets();
     }
 
+    /**
+     *
+     * @param type
+     * @return the cost of a single ticket for the provided ticket Type
+     */
     private int getTicketCost(TicketTypeRequest.Type type) {
         return switch (type) {
             case INFANT -> INFANT_TICKET_COST;
